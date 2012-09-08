b626ed78732b95f8c0b440a37137ca2196d8193a
Dobri Dobrev <dobridobrev@dyn-209-2-235-148.dyn.columbia.edu>
2012-09-08 15:42:58 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,3 @@
+A small JQuery based app using JSON to pull 10 most relevant photos for a user defined location from the Flicker API.
+For understandable reasons I have deleted my flickr API key. In order for the app to work, add your API key at line 41.
+Code is free to be stolen/reimplemented/ fiddled with in any way possible. I do not take any responsibility for its actions ;p
---
+++ proto.js
@@ -0,0 +1,46 @@
+function query(){
+	var name= $("#POI").val();
+	var lat= $("#lat").val();
+	var lon= $("#long").val();
+	//console.log(lon);
+	var urlJSON= createURL(name, lat, lon);
+	console.log(urlJSON);
+	$.getJSON(urlJSON, displayImages);
+}
+function displayImages(data) {
+	console.log($("#textId").val());
+	var photos = document.getElementById('photos');
+	console.log(data.query.results.photo.length);
+	for(var i=0; i<data.query.results.photo.length; i++){
+		var ph=data.query.results.photo[i];
+		var url="http://farm"+ph.farm+".staticflickr.com/"+ph.server+"/"+ph.id+"_"+ph.secret+".jpg";
+		var oImg= createImg(url);
+		photos.appendChild(oImg);
+	}
+
+   
+}
+function createImg(url){
+	var img= document.createElement("img");
+	img.setAttribute('src', url);
+	img.setAttribute('alt', "fail");
+	img.setAttribute('width', "300");
+	img.setAttribute('height', "300");
+	return img;
+}
+function createURL(name, lat, lon){
+	
+	name=name.replace(/\s/g, "%20");
+	var urlJ= "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.search%20where%20has_geo%3D%22true%22%20and%20text%3D%22";
+	urlJ+=name;
+	urlJ+="%22%20and%20lat%3D%22";
+	urlJ+=lat;
+	urlJ+="%22%20and%20lon%3D%22-";
+	urlJ+=lon;
+	urlJ+="%22%20and%20api_key%3D%";
+	urlJ+="APIKEY";
+	urlJ+="%22%20and%20sort%3D%22relevance%22%20and%20media%3D%22photos%22%20and%20radius%3D%222%22&format=json&diagnostics=true&callback=?";
+	//console.log(urlJ);
+	return urlJ;
+}
+
